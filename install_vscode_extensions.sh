a5ead92342157dc42f4489ae7a39cab0d344b45a
Marius Butuc <marius.butuc@gmail.com>
2021-11-01 09:39:08 -0400

    Update VS Code extensions list

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -2,21 +2,29 @@
 
 # code --list-extensions
 declare -a extensions=(
-  "arcanis.vscode-zipfs"
   "dbaeumer.vscode-eslint"
   "dsznajder.es7-react-js-snippets"
   "eamodio.gitlens"
+  "EditorConfig.EditorConfig"
   "esbenp.prettier-vscode"
   "GraphQL.vscode-graphql"
   "Gruntfuggly.todo-tree"
+  "JakeBecker.elixir-ls"
   "mechatroner.rainbow-csv"
+  "ms-azuretools.vscode-docker"
+  "ms-python.python"
+  "ms-python.vscode-pylance"
+  "ms-toolsai.jupyter"
+  "ms-toolsai.jupyter-keymap"
+  "ms-toolsai.jupyter-renderers"
   "ms-vsliveshare.vsliveshare"
   "ms-vsliveshare.vsliveshare-audio"
   "ms-vsliveshare.vsliveshare-pack"
-  "msjsdiag.debugger-for-chrome"
   "Orta.vscode-jest"
-  "stkb.rewrap"
+  "piotrpalarz.vscode-gitignore-generator"
   "tokoph.ghosttext"
+  "tomoki1207.pdf"
+  "wix.vscode-import-cost"
 )
 
 for extension in "${extensions[@]}"
